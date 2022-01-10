1e514f83f10b7fcf2d182a70f128205eaed64145
Renato Nogueira da Silva <renato1987geofisica@outlook.com>
2022-01-10 20:32:39 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg'
+import './style.css'
+
+
+
+function Navbar(){
+
+    return(
+        <header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/Nogueirarenato" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/NogueiraRenato</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
+    );
+
+}
+
+export default Navbar;

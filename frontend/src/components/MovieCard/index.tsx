d1d8d2467fc3af17c45243eeff449d63932763f3
Renato Nogueira da Silva <renato1987geofisica@outlook.com>
2022-01-16 19:00:44 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -7,17 +7,17 @@ import './styles.css';
 type Props = {
     movie: Movie;
 }
-function MovieCard({movie} : Props) {
+function MovieCard({ movie }: Props) {
 
-    
+
     return (
-        
+
 
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore count={movie.count} score={movie.score}/>
+                <MovieScore count={movie.count} score={movie.score} />
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,18 +1,18 @@
 import './styles.css';
-import {ReactComponent as Starfull} from 'assets/img/star-full.svg';
-import {ReactComponent as StarHalf} from 'assets/img/star-half.svg';
-import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
+import { ReactComponent as Starfull } from 'assets/img/star-full.svg';
+import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
+import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
 
-type Props ={
+type Props = {
   score: number;
 }
 
 type StarProps = {
   fill: number;
 }
-// EX:
-// getFills(3.5) => [1, 1, 1, 0.5, 0]
-// getFills(4.1) => [1, 1, 1, 1, 0.5]
+// // // EX:
+// // // getFills(3.5) => [1, 1, 1, 0.5, 0]
+// // // getFills(4.1) => [1, 1, 1, 1, 0.5]
 function getFills(score: number) {
 
   const fills = [0, 0, 0, 0, 0];
@@ -30,30 +30,32 @@ function getFills(score: number) {
 
   return fills;
 }
- function Star({fill} : StarProps){
+
+//seleciona se a estrela é cheia, vazia ou média
+function Star({ fill }: StarProps) {
   if (fill === 0) {
     return <StarEmpty />
   }
   else if (fill === 1) {
     return <Starfull />
   }
-  else{
+  else {
     return <StarHalf />
   }
- }
+}
 
-function MovieStars({score}:Props){
+function MovieStars({ score }: Props) {
   const fills = getFills(score);
 
-    return(
-      <div className="dsmovie-stars-container">
-          <Star fill={fills[0]} />
-          <Star fill={fills[1]} />
-          <Star fill={fills[2]} />
-          <Star fill={fills[3]} />
-          <Star fill={fills[4]} />
-      </div>  
-    );
+  return (
+    <div className="dsmovie-stars-container">
+      <Star fill={fills[0]} />
+      <Star fill={fills[1]} />
+      <Star fill={fills[2]} />
+      <Star fill={fills[3]} />
+      <Star fill={fills[4]} />
+    </div>
+  );
 
 }
 export default MovieStars;
